85a35e816323d7e76609768369f4a53e7f6ce53b
mortee199918 <mortejauroldan@gmail.com>
2026-05-27 14:49:22 +0200

    arreglos amb ramon

--- main.py
+++ main.py
@@ -2,10 +2,20 @@
 from fastapi import FastAPI
 from routes.bubbletea import router as bubbletea_router
 from routes.auth import router as auth_router
-
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Bubble Tea API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],
+    allow_origin_regex=r"https?://(localhost|127.0.0.1)(:\d+)?$",
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
+
+
 app.include_router(bubbletea_router)
 app.include_router(auth_router)
 
